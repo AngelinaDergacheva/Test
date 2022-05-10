823d0f73d1c6c6a5fa4b749298b5faff7b1845b0
Anna_Krivonogih <anya_k@yandex.ru>
2022-05-10 22:58:15 +0300

    file add

---
+++ Stroka/Stroka/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Stroka
+{
+    class Program
+    {
+        private string str = "";
+        private int UpperCount = 0;
+        private int LowerCount = 0;
+        public string vvod()
+        {
+            Console.WriteLine("Введите строку");
+            str = Console.ReadLine();
+            return str;
+        }
+        public void poisk()
+        {
+            foreach (var symbol in str)
+            {
+                if (char.IsUpper(symbol))
+                    UpperCount++;
+                else
+                    if (char.IsLower(symbol))
+                    LowerCount++;
+              
+            }
+            Console.WriteLine("Количество строчных символов = " + LowerCount+ " Количество прописных символов = "+ UpperCount);
+        }
+        static void Main(string[] args)
+        {
+          
+            Program p = new Program();
+            Console.WriteLine("Ваща строка: "+ p.vvod());
+            p.poisk();
+        }
+    }
+}
